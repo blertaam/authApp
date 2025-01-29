466f15b4eef7685d42ebd550a59428b4b63aa8a2
blertaam <mehmetiblerta3@gmail.com>
2025-01-29 17:46:50 +0100

    blerta

---
+++ screens/Welcome.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+
+const Welcome =({route})=>{
+    const{username}=route.params || {}
+    return(<View style={style.container}>
+           <Text style={style.title}> Welcome {username ? `, ${username}` :""} </Text>
+           <Text>This is the second screen where we show the logged-in users</Text>
+           
+    </View>)
+}
+
+const style= StyleSheet.create({
+    container:{
+        flex:1,
+        paddingTop:80,
+        paddingHorizontal:20,
+        backgroundColor: '#fff'
+    },
+    title:{
+        fontSize:24,
+        marginBottom:20,
+        textAlign: "center"
+    }
+})
+
+
+
+export default Welcome;
